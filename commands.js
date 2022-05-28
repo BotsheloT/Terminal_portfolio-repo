b97601854fc587b97513549d92f0a513e2478507
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-05-28 22:48:31 +0200

    More base code

--- commands.js
+++ commands.js
@@ -1,3 +1,11 @@
+//Links for socials
+let github = 'https://github.com/BotsheloT';
+let linkedin = 'https://www.linkedin.com/in/botshelo-tlhabanyane-36b573210/';
+let behance = 'https://www.behance.net/botsheltlhaban2/projects';
+let pinterest = 'https://za.pinterest.com/btlhabanyane/_saved/';
+let instagram = 'https://www.instagram.com/buttermilkmoses/';
+
+//Arrays for command results
 about = [
 
 ];
@@ -19,7 +27,7 @@ help = [
 ];
 
 project = [
-
+    'Still currating...most projects are offline, on Github or Top Secret'
 ];
 
 socials = [
