d372a2d3a293b8dd6138b95806efa73c0654ccc3
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-05-28 22:12:36 +0200

    Additional boilerplate

---
+++ sandbox.js
@@ -0,0 +1,24 @@
+//variable for user input
+let cmd = document.querySelector('.cmd');
+
+//function for command entry
+let command = () => {
+    switch(cmd){
+        case'about':
+        
+        case'banner':
+        
+        case'clear':
+        
+        case'email':
+        
+        case'help':
+        
+        case'projects':
+        
+        case'socials':
+        
+        default:
+
+    }
+}
