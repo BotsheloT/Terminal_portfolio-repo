e016f431228f51ac159cd7ed4fbe54f1557ac191
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-05-31 09:25:32 +0200

    Flow changes

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,37 @@
 //Variable for user input
-let cmd = document.querySelector('.cmd').innerHTML;
+let cmd = document.querySelector('.cmd');
 
 //Function for command entry
 let command = (ucmd) => {
     switch(ucmd.toLowerCase()) {
         case'about':
         about;
+        ucmd.clear();
+        break;
         case'banner':
         banner;
+        ucmd.clear();
+        break;
         case'clear':
         clear;
+        ucmd.clear();
+        break;
         case'email':
         email;
+        ucmd.clear();
+        break;
         case'help':
         helper();
+        ucmd.clear();
+        break;
         case'projects':
         projects;
+        ucmd.clear();
+        break;
         case'socials':
         socials;
+        ucmd.clear();
+        break;
         default:
         }
     }
@@ -25,7 +39,7 @@ let command = (ucmd) => {
 //Event listener for input 
 cmd.addEventListener('keyup', (e) =>{
     if(e.keyCode === 13){
-        command(cmd)
+        command(cmd.innerHTML);
     }
 })
 
